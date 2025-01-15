8686fed70d52217795f370c91885e61d55a2c516
GautamAggarwalDev <gautam.aggarwal.tech@gmail.com>
2025-01-15 12:51:32 +0530

    finalise the express app

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 const express = require('express');
-const server = require('./config/serverConfig.js')
+const server = require('./config/serverConfig.js');
+const connectDB = require('./config/dbConfig.js');
+const bodyParser = require('body-parser')
 
 const app = express();
 
-// const PORT = process.env.PORT;
+app.use(express.json());
+app.use(express.text());
+app.use(express.urlencoded({extended: true}));
 
+app.post('/ping', (req, res) => {
+    console.log(req.body);
+    return res.json({message: "pong"})
+});
 
-app.listen(server.PORT, () => {
+app.listen(server.PORT, async() => {
+    await connectDB();
     console.log(`Server is started at ${server.PORT}...`);
 }); 
+
